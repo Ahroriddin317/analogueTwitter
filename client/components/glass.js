5fa87eb496647963af972b4e2031a2bd34b0d988
Student <ahroriddin0478@gmail.com>
2021-09-19 15:29:11 +0500

    added top.js component for show a popular posts

--- client/components/glass.js
+++ client/components/glass.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from './navigation.js';
 import Posts from './posts.js';
+import Top from './top.js';
 
 const Glass = () => {
   return (
     <div className="glass">
       <Navigation />
       <Posts />
+      <Top />
     </div>
   )
 }
---
+++ client/components/top.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Post from './post';
+
+const Top = () => {
+  const { posts } = useSelector(s => s.analogueTwitter);
+  const topPosts = posts.sort((a, b) => b.likes - a.likes).slice(0, 5);
+  return(
+    <div className="top" >
+      <h1>Popular posts</h1>
+      {topPosts.map(({ id, content, likes, author}) => {
+        return(
+          <div key={id}>
+            <h1>{author.name}</h1>
+            <p>{content}</p>
+            <img src="../assets/svg/activeLike.svg" width="20px" alt="like icon" />
+            <h6>{likes}</h6>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Top
