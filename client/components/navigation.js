539581434dcbb2c5f00a944ee0501ca940f6fe06
Student <ahroriddin0478@gmail.com>
2021-09-21 21:13:25 +0500

    changed navigation styles and navigation.js component

--- client/components/navigation.js
+++ client/components/navigation.js
@@ -4,20 +4,21 @@ import { history } from '../redux';
 
 const Navigation = () => {
   const { user } = useSelector(s => s.analogueTwitter)
+  const pathname = useSelector(s => s.router.location.pathname).indexOf('profile')
   return (
     <nav className="nav">
       <ul>
         <li>
-          <button className="nav_button" onClick={() => history.push(`/${user.id}`)}>
+          <button className={`nav_button ${pathname === -1 ? 'active' : ''}`} onClick={() => history.push(`/${user.id}`)}>
             <img src="../assets/svg/list.svg" alt="posts" width="30px" height="30px" />
+            posts
           </button>
-          posts
         </li>
         <li>
-          <button className="nav_button" onClick={() => history.push(`/${user.id}/profile`)}>
+          <button className={`nav_button ${pathname !== -1 ? 'active' : ''}`} onClick={() => history.push(`/${user.id}/profile`)}>
             <img src="../assets/svg/user.svg" alt="posts" width="30px" height="30px" />
+            Profile
           </button>
-          profile
         </li>
       </ul>
     </nav>
